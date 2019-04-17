89d48a2fbaac746ba1d63536f3502dfe6e5c98ac
DariaYudina <39306102+DariaYudina@users.noreply.github.com>
2019-04-17 12:02:47 +0400

    jast save

--- src/main/java/com/unit/Main.java
+++ src/main/java/com/unit/Main.java
@@ -5,24 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         DBconnection.getInstance();
-       /*
-       try{
-           ResultSet resultSet = DataBase.getInstance().query("SELECT * FROM Users");
-           while(resultSet.next()){
-
-               String usernames = resultSet.getString("username");
-               String emails = resultSet.getString("email");
-               String phones = resultSet.getString("phone");
-               //int price = resultSet.getInt("Price");
-
-               System.out.printf("Name: %s; email: %s; phone: %s\n",usernames, emails, phones);
-           }
-       }
-       catch (Exception ex){
-           System.out.println("query failed...");
-           System.out.println(ex);
-       }
-      */
         boolean menu = true;
         Scanner in = new Scanner(System.in);
         while(menu){
@@ -43,18 +25,38 @@ public class Main {
                 }
             }
             switch (choice){
-                case 1: System.out.println("вы  выбрали 1"); menu = false; break;
-                case 2: System.out.println("вы  выбрали 2"); menu = false; break;
-                case 3: System.out.println("вы  выбрали 3"); menu = false; break;
-                case 4: System.out.println("вы  выбрали 5"); menu = false; break;
+                case 1: System.out.println("вы  выбрали 1");
+
+                    break;
+                case 2: System.out.println("вы  выбрали 2"); break;
+                case 3: System.out.println("вы  выбрали 3"); break;
+                case 4: System.out.println("вы  выбрали 5"); break;
                 case 5: System.out.println("Выбрано выйти"); menu = false; break;
                 default:
                     System.out.println("Выберите существующий пункт меню");
                     break;
             }
        }
-
+        DBconnection.closeConnection();
     }
 }
 
 
+       /*
+       try{
+           ResultSet resultSet = DataBase.getInstance().query("SELECT * FROM Users");
+           while(resultSet.next()){
+
+               String usernames = resultSet.getString("username");
+               String emails = resultSet.getString("email");
+               String phones = resultSet.getString("phone");
+               //int price = resultSet.getInt("Price");
+
+               System.out.printf("Name: %s; email: %s; phone: %s\n",usernames, emails, phones);
+           }
+       }
+       catch (Exception ex){
+           System.out.println("query failed...");
+           System.out.println(ex);
+       }
+      */
